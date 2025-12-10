98809cec86b3b99bd1559f3e82fed9484905b5e8
Rodrigo Garcez Hermann <rodrigohermann18@outlook.com>
2025-12-09 23:18:03 -0300

    feat: add AWS VPC module with subnets and internet gateway

---
+++ modules/aws/vpc/README.md
@@ -0,0 +1,13 @@
+# AWS VPC Module
+
+Creates a simple VPC with public subnets, internet gateway and route table.
+
+Inputs
+- `cidr_block` (string) - VPC CIDR
+- `public_subnets` (list) - list of public subnet CIDRs
+- `availability_zones` (list) - AZs for subnets
+- `tags` (map) - tags
+
+Outputs
+- `vpc_id`
+- `public_subnet_ids`
---
+++ modules/aws/vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "cidr_block" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of AZs for subnets"
+  type        = list(string)
+  default     = ["us-east-1a"]
+}
+
+variable "tags" {
+  description = "Tags map"
+  type        = map(string)
+  default     = {}
+}
