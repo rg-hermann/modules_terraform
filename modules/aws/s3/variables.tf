5307dd2c63cdb662bb9e443968d660f5c582374c
Rodrigo Garcez Hermann <rodrigohermann18@outlook.com>
2025-12-09 23:17:40 -0300

    feat: add AWS S3 module with encryption and versioning

---
+++ modules/aws/s3/variables.tf
@@ -0,0 +1,28 @@
+variable "bucket" {
+  description = "S3 bucket name"
+  type        = string
+}
+
+variable "acl" {
+  description = "ACL for the bucket"
+  type        = string
+  default     = "private"
+}
+
+variable "versioning" {
+  description = "Enable versioning"
+  type        = bool
+  default     = true
+}
+
+variable "force_destroy" {
+  description = "Allow bucket deletion with objects"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "Map of tags"
+  type        = map(string)
+  default     = {}
+}
